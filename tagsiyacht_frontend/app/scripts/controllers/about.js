2a6d4ae913d28be58d993259e7f3eaf25f745105
Christian Bouvier <cbouvier@trea.uy>
2015-05-06 19:27:45 -0300

    tagsiyacht_frontend ready

---
+++ tagsiyacht_frontend/app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name tagsiyachtFrontendApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the tagsiyachtFrontendApp
+ */
+angular.module('tagsiyachtFrontendApp')
+  .controller('AboutCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
---
+++ tagsiyacht_frontend/app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name tagsiyachtFrontendApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the tagsiyachtFrontendApp
+ */
+angular.module('tagsiyachtFrontendApp')
+  .controller('MainCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
