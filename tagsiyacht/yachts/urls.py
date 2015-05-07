05c188efcc5011f4f9489fb4490bc9af2251850a
Alvaro Larrosa <alarrosa14@gmail.com>
2015-05-06 21:27:58 -0300

    Añadido el proxy para buena comunicación entre frontend y backend.

--- tagsiyacht/yachts/urls.py
+++ tagsiyacht/yachts/urls.py
@@ -1,9 +1,10 @@
+from django.views.decorators.csrf import ensure_csrf_cookie
 from django.conf.urls import url
 from yachts import views
 
 urlpatterns = [
-    url(r'^yachts/$', views.YachtList.as_view()),
-    url(r'^yachts/(?P<pk>[0-9]+)/$', views.YachtDetail.as_view()),
-    url(r'^clients/$', views.ClientList.as_view()),
-    url(r'^clients/(?P<pk>[0-9]+)/$', views.ClientDetail.as_view()),
+    url(r'^rest/yachts/$', ensure_csrf_cookie(views.YachtList.as_view())),
+    url(r'^rest/yachts/(?P<pk>[0-9]+)/$', views.YachtDetail.as_view()),
+    url(r'^rest/clients/$', views.ClientList.as_view()),
+    url(r'^rest/clients/(?P<pk>[0-9]+)/$', views.ClientDetail.as_view()),
 ]
